1bbb49e9b30b60506d19049e6d164eb41c5495e3
cjfal2 <cjfal2@kakao.com>
2022-10-29 22:52:46 +0900

    1029 2

---
+++ 백준/Gold/[G2] 19236 촉법상어.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+
+
+news = [[],[-1, 0],[-1, -1],[0, -1],[1, -1],[1, 0],[1, 1],[0, 1],[-1, 1]]
+
+def dfs(xx, yy, su, ocean):
+    global co
+    co = max(co, su)
+    shark_d = ocean[xx][yy][1]
+    ocean[xx][yy][0] = "S"
+
+    for target in range(1, 17):
+        flag = False
+        for x in range(4):
+            for y in range(4):
+                if ocean[x][y][0] == target:
+                    fish_x, fish_y = x, y
+                    flag = True
+                    break
+            if flag:
+                break
+        if flag:
+            dix = ocean[fish_x][fish_y][1] - 1
+            while 1:
+                dix += 1
+                if dix > 8:
+                    dix = 1
+                nx = fish_x + news[dix][0]
+                ny = fish_y + news[dix][1]
+                if 0 <= nx < 4 and 0 <= ny < 4 and ocean[nx][ny][0] != "S":
+                    ocean[fish_x][fish_y][1] = dix
+                    ocean[fish_x][fish_y], ocean[nx][ny] = ocean[nx][ny], ocean[fish_x][fish_y]
+                    break
+
+    ocean[xx][yy][0] = 0
+
+    dx, dy = news[shark_d][0], news[shark_d][1]
+    nx, ny = xx, yy
+    for _ in range(3):
+        nx += dx
+        ny += dy
+        if 0 <= nx < 4 and 0 <= ny < 4 and ocean[nx][ny][0]:
+            dfs(nx, ny, su+ocean[nx][ny][0], deepcopy(ocean))
+
+
+sea = [[] for _ in range(4)]
+for i in range(4):
+    a,aa,b,bb,c,cc,d,dd = map(int, input().split())
+    for A,B in [(a,aa),(b,bb),(c,cc),(d,dd)]:
+        sea[i].append([A, B])
+co = 0
+
+dfs(0, 0, sea[0][0][0], sea)
+print(co)
