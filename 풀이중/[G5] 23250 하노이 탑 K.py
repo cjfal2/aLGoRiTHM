f9e1496b4a5649473036c009afaa902a5f78aa8a
cjfal2 <cjfal2@kakao.com>
2022-11-02 21:39:34 +0900

    1102

--- 풀이중/[G5] 23250 하노이 탑 K.py
+++ 풀이중/[G5] 23250 하노이 탑 K.py
@@ -1,13 +1,15 @@
+from collections import deque
+
 def hanoi(NUM, X, Y):
     if NUM > 1:
         hanoi(NUM-1, X, 6-X-Y)
     L.append((X, Y))
     if len(L) == k:
-        print(*L[-1])
+        print(L.pop())
         quit()
     if NUM > 1:
         hanoi(NUM-1, 6-X-Y, Y)
 
 n, k = map(int, input().split())
-L = []
+L = deque()
 hanoi(n,1,3) # X
