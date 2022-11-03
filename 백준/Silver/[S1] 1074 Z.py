69c248c67c52073f05d80eae1ee71f995160d68a
cjfal2 <cjfal2@kakao.com>
2022-11-03 20:57:49 +0900

    1103

--- 백준/Silver/[S1] 1074 Z.py
+++ 백준/Silver/[S1] 1074 Z.py
@@ -1,7 +1,23 @@
 def Z(n, x, y):
-    if n == 0:
-        return 0
-    return 2*(x%2)+(y%2) + 4*Z(n-1, x//2, y//2)
-
+    global co
+    if x==r and y==c:
+        print(co)
+        quit()
+    if n == 1:
+        co += 1
+        return
+    if not (x<=r<x+n and y<=c<y+n):
+        co += n**2
+        return
+    m = n//2
+    Z(m,x,y) # 1 사분면
+    Z(m,x,y+m) # 2
+    Z(m,x+m,y) # 3
+    Z(m,x+m,y+m) # 4
+    
+    
 N, r, c = map(int, input().split())
-print(Z(N, r, c))
+co = 0
+Z(2**N,0,0)
+# 배열의 크기를 먼저 넣어주고
+# 반씩 줄여가면서 그곳에서 다시 1,2,3,4분면을 확인
