85f850783ea89e84566e504e02db822db7b8f05a
kimdongjun <cjfal2@kakao.com>
2022-11-04 19:56:20 +0900

    [Silver II] Title: 침투, Time: 1272 ms, Memory: 43072 KB -BaekjoonHub

---
+++ 백준/Silver/13565. 침투/침투.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+pan = [list(map(int, list(input()))) for _ in range(N)]
+
+visited = [[False for _ in range(M)] for _ in range(N)]
+flag = False
+for i in range(N):
+    for j in range(M):
+        if not i and not visited[i][j]:
+            visited[i][j] = True
+            q = [(i, j)]
+            while q:
+                x, y = q.pop(0)
+                for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                    nx, ny = x + dx, y + dy
+                    if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny] and not pan[nx][ny]:
+                        if nx == N-1:
+                            print("YES")
+                            quit()
+                        visited[nx][ny] = True
+                        q.append((nx, ny))
+print("NO")
