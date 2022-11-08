9cddba5fcc32547d0dff3a422a25aebb45a817e7
kimdongjun <cjfal2@kakao.com>
2022-11-08 20:23:23 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,55 @@
+z=[0 for _ in range(26)]
+for W in input():
+    if W=='a':
+        z[0] += 1
+    elif W=='b':
+        z[1] += 1
+    elif W=='c':
+        z[2] += 1
+    elif W=='d':
+        z[3] += 1
+    elif W=='e':
+        z[4] += 1
+    elif W=='f':
+        z[5] += 1
+    elif W=='g':
+        z[6] += 1
+    elif W=='h':
+        z[7] += 1
+    elif W=='i':
+        z[8] += 1
+    elif W=='j':
+        z[9] += 1
+    elif W=='k':
+        z[10] += 1
+    elif W=='l':
+        z[11] += 1
+    elif W=='m':
+        z[12] += 1
+    elif W=='n':
+        z[13] += 1
+    elif W=='o':
+        z[14] += 1
+    elif W=='p':
+        z[15] += 1
+    elif W=='q':
+        z[16] += 1
+    elif W=='r':
+        z[17] += 1
+    elif W=='s':
+        z[18] += 1
+    elif W=='t':
+        z[19] += 1
+    elif W=='u':
+        z[20] += 1
+    elif W=='v':
+        z[21] += 1
+    elif W=='w':
+        z[22] += 1
+    elif W=='x':
+        z[23] += 1
+    elif W=='y':
+        z[24] += 1
+    elif W=='z':
+        z[25] += 1
+print(*z)
