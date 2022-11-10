fcd757e84489ea416b66c40e5d2b97c5331e2f14
kimdongjun <cjfal2@kakao.com>
2022-11-10 22:47:24 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 312 ms, Memory: 126116 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -1,85 +1,97 @@
-from copy import deepcopy
+'''
+1. N,M,K 입력
+2. M번 반복으로 파이어볼 정보 입력
+3. 파이어볼 정보는 fireballs[]에 저장.
+?? "한 점에 파이어볼 두 개 이상이 있다"라는 것을 어떻게 알까?
+- arr(각 칸은 [])를 만들어 직접 파이어볼을 넣고 움직여서 브루트 포스로 length가 2이상인 arr를 뽑는다?
+- fireballs를 for문 돌며 ... no
 
-news = {
-    0: [-1, 0],
-    1: [-1, 1],
-    2: [0, 1],
-    3: [1, 1],
-    4: [1, 0],
-    5: [1, -1],
-    6: [0, -1],
-    7: [-1, -1]
-}
 
-def jojeol(x, y):
-    if x == N:
-        x = 0
-    elif x == -1:
-        x = N-1
-    if y == N:
-        y = 0
-    elif y == -1:
-        y = N-1
-    return x, y
+for K 로 move()
+move 후 for 문으로 fireballs를 돌며 arr[(N*N)]에 각 칸에 fireball정보 append
+for 끝난 후 브루트포스로 length가 2이상인 칸에서 gather()
+* gather전에 same 등의 이름으로 더미데이터 True 생성,
+for 문으로 2 이상의 칸 돌며 홀짝 여부가 전 것과 같다면 pass 아니라면 same을 False로 토글
+if 질량합//5로 가지치고
+
+if same:
+0246으로 네 개의 파이어볼 fireballs에 append,
+else:
+1357으로 네 개의 파이어볼 fireballs에 append
+fireballs에서 기존의 합쳐진 파이어볼들 remove
+'''
+
+def move():
+    global fireballs, arr
+    directions=[(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+    for i in fireballs:
+        i[0] += directions[i[4]][0]*i[3]
+        i[1] += directions[i[4]][1] * i[3]
+        i[0] = (i[0]) % N
+        i[1] = (i[1]) % N
+        arr[i[0]][i[1]].append(i)
+
+
+def gather():
+    global fireballs, arr
+    new_fire = []
+    for i in arr:
+        for j in i:
+            if 1 > len(j):
+                continue
+            elif len(j) == 1:
+                new_fire.append(j[0])
+            else:
+                mass = 0
+                speed = 0
+                same = True
+                for k in range(len(j)):
+                    mass += j[k][2]
+                    speed += j[k][3]
+                    if k == 0:
+                        if j[k][4] in odd:
+                            standard = odd
+                        else:
+                            standard = even
+                        row = j[k][0]
+                        col = j[k][1]
+                    else:
+                        if j[k][4] not in standard:
+                            same = False
+                mass_avg = mass // 5
+                if mass_avg:
+                    speed_avg = speed // len(j)
+                    if same:
+                        new_fire.append([row, col, mass_avg, speed_avg, 0])
+                        new_fire.append([row, col, mass_avg, speed_avg, 2])
+                        new_fire.append([row, col, mass_avg, speed_avg, 4])
+                        new_fire.append([row, col, mass_avg, speed_avg, 6])
+                    else:
+                        new_fire.append([row, col, mass_avg, speed_avg, 1])
+                        new_fire.append([row, col, mass_avg, speed_avg, 3])
+                        new_fire.append([row, col, mass_avg, speed_avg, 5])
+                        new_fire.append([row, col, mass_avg, speed_avg, 7])
+    return new_fire
 
 N, M, K = map(int, input().split())
-base = [[[] for _ in range(N)] for _ in range(N)]
-pan = [[[] for _ in range(N)] for _ in range(N)]
-deca = []
-for _ in range(M):
-    r,c,m,s,d = list(map(int, input().split()))
-    pan[r-1][c-1].append([m,s,d])  # m s d = 질량 이동량 방향
-# for v in pan:
-#     print(v)
-# print("----------------")
-for _ in range(K):
-    newbase = deepcopy(base)
-    
-    for i in range(N):
-        for j in range(N):
-            if pan[i][j]:
-                for kk in pan[i][j]:
-                    m, s, d = kk
-                    dx, dy = news.get(d)
-                    nx, ny = i, j
-                    for _ in range(s):
-                        nx += dx
-                        ny += dy
-                        nx, ny = jojeol(nx, ny)
-                    newbase[nx][ny].append([m,s,d])
+fireballs = []
+arr = [[[] for __ in range(N)] for _ in range(N)]
+odd = [1, 3, 5, 7]
+even = [0, 2, 4, 6]
+for balls in range(M):
+    fireballs.append(list(map(int, input().split())))
+for ball in fireballs:
+    ball[0] -= 1
+    ball[1] -= 1
 
-    pan = deepcopy(newbase)
-    # for v in pan:
-    #     print(v)
-    # print("--------")
-    for h in range(N):
-        for w in range(N):
-            if len(pan[h][w]) > 1:
-                new_m, new_s, check = 0, 0, set()
-                for m, s, d in pan[h][w]:
-                    new_m += m
-                    new_s += s
-                    check.add(d%2)
-                new_m //= 5
-                if new_m == 0:
-                    pan[h][w] = []
-                    continue
-                new_s //= len(pan[h][w])
-                if len(list(check)) == 1:
-                    dix = [0,2,4,6]
-                else:
-                    dix = [1,3,5,7]
-                pan[h][w] = []
-                for z in dix:
-                    pan[h][w].append([new_m, new_s, z])
-                
-    # for v in pan:
-    #     print(v)
-    # print("---@@@@@-----")
+for moves in range(K):
+    move()
+
+    fireballs = gather()
+
+    arr = [[[] for __ in range(N)] for _ in range(N)]
 ans = 0
-for x in range(N):
-    for y in range(N):
-        if pan[x][y]:
-            for k in pan[x][y]:
-                ans += k[0]
+for item in fireballs:
+    ans += item[2]
+
 print(ans)
