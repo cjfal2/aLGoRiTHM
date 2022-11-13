edbbe42488d4358b3ef4a3ef81a8ca2a4dff428d
kimdongjun <cjfal2@kakao.com>
2022-11-13 12:21:01 +0900

    [Gold V] Title: 상범 빌딩, Time: 188 ms, Memory: 32524 KB -BaekjoonHub

---
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs(sero1, garo1):
+    visited = []
+    for _ in range(high):
+        visited1 = [[0 for _ in range(sero1)] for _ in range(garo1)]
+        visited.append(visited1)
+    hh, gg, ss = start
+    q = deque()
+    visited[hh][gg][ss] = 1
+    q.append((hh, gg, ss))
+    while q:
+        v, x, y = q.popleft()
+        for dh, dx, dy in [[0, 0, 1], [0, 1, 0], [0, 0, -1], [0, -1, 0], [1, 0, 0], [-1, 0, 0]]:
+            nh = v + dh
+            nx = x + dx
+            ny = y + dy
+            if high > nh >= 0 and garo1 > nx >= 0 and sero1 > ny >= 0 and visited[nh][nx][ny] == 0:
+                if building[nh][nx][ny] == ".":
+                    visited[nh][nx][ny]=visited[v][x][y] + 1
+                    q.append((nh, nx, ny))
+                elif building[nh][nx][ny] == "E":
+                    print(f"Escaped in {visited[v][x][y]} minute(s).")
+                    return
+    print("Trapped!")
+
+
+while 1:
+    high, g, s = map(int, input().strip().split())
+    if high == g == s == 0:
+        break
+    building = [[] for _ in range(high)]
+    start = []
+    for u in range(high):
+        for k in range(g+1):
+            a = list(input().strip())
+            if a == []:
+                break
+            for aa in range(len(a)):
+                if a[aa] == "S":
+                    start = (u, k, aa)
+            building[u].append(a)
+    bfs(s, g)
