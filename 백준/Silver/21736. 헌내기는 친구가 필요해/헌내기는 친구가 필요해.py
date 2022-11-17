a0bc1292d41273d3e97f0182c735e80bfd14124f
kimdongjun <cjfal2@kakao.com>
2022-11-17 20:20:47 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 1696 ms, Memory: 39360 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+pan = []
+flag = 0
+for b in range(N):
+    a = list(input())
+    pan.append(a)
+    for aa in range(M):
+        if a[aa] == "I":
+            s, e = b, aa
+            pan[s][e] = "X"
+visited = [[False for _ in range(M)] for _ in range(N)]
+visited[s][e] = True
+q = [(s, e)]
+ans = 0
+while q:
+    x, y = q.pop(0)
+    for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        nx, ny = x + dx, y + dy
+        if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny]:
+            if pan[nx][ny] != "X":
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                if pan[nx][ny] == "P":
+                    ans += 1
+if ans:
+    print(ans)
+else:
+    print("TT")
