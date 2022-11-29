dc9dfd625916eae268b997602a36cf34f256069b
kimdongjun <cjfal2@kakao.com>
2022-11-29 15:10:00 +0900

    [Silver II] Title: 너구리 구구, Time: 100 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.py
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.py
@@ -1,28 +1,30 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
 def bfs():
     global MAX
     visited = [0 for _ in range(N+1)]
     visited[1] = 1
-    q = [(1, 0)]
+    q = deque()
+    q.append((1, 0))
     while q:
-        t, k = q.pop(0)
+        t, k = q.popleft()
         if visited[t]:
-            # print(k)
             MAX = max(MAX, k)
         for e, w in G[t]:
             if not visited[e]:
                 visited[e] = 1
                 q.append((e, k+w))
-                # print(k+w)
 
 
-
-N = int(input())
+N = int(input().strip())
 G = [[] for _ in range(N+1)]
 for number in range(N-1):
-    start, end, meter = map(int, input().split())
+    start, end, meter = map(int, input().strip().split())
     G[start].append((end, meter))
     G[end].append((start, meter))
-# print(G)
+
 MAX = 0
 bfs()
 print(MAX)
