d726f5c69eecf8269a5c559a4d0e75f0d8a858fd
kimdongjun <cjfal2@kakao.com>
2022-11-30 15:08:45 +0900

    [Silver II] Title: 수익, Time: 300 ms, Memory: 114716 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.py
+++ 백준/Silver/4097. 수익/수익.py
@@ -1,17 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+
 while 1:
     N = int(input().strip())
-    if not N:
-        break
-    MAX = -999999
-    SUM = 0
+    if not N: break
+    MAX, SUM = -99999, 0
     for _ in range(N):
         num = int(input().strip())
-        if SUM + num > num:
-            SUM = SUM + num
-        else:
-            SUM = num
+        SUM = SUM + num if SUM > 0 else num
         MAX = max(MAX, SUM)
     print(MAX)
