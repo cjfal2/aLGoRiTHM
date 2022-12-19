05cf1bdeb19ce3294653482c2c9d385e800bcf75
kimdongjun <cjfal2@kakao.com>
2022-12-20 01:03:24 +0900

    [Gold IV] Title: 뱀, Time: 64 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -22,67 +22,61 @@
 Q. 사과의 위치와 뱀의 이동경로가 주어질 때 이 게임이 몇 초에 끝나는지 계산하라.
 '''
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-base = [[0 for _ in range(N)] for _ in range(N)]
+base = [[0 for _ in range(N)] for _ in range(N)] # 판을 만듦
 
 for _ in range(int(input())):
    a, b = map(int, input().split())
-   base[a-1][b-1] = 2
+   base[a-1][b-1] = 2 # 인덱스 맞추기 위해 1씩 빼줘서 2를 저장 (2 = 사과)
 
-info = [] # 시간, 회전
+info = [] # [시간, 회전]
 for _ in range(int(input())):
    a, b = list(input().split())
    info.append([int(a), b])
-info.sort()
+info.sort() # 혹시 시간이 뒤죽박죽 올까봐 sort
 
-# for v in base:
-#    print(v)
-# print(info)
-
-time = 0
-dix = 0 # 0우 1하 2좌 3상
+time = 0 # 시간 변수
+dix = 0 # 0우 1하 2좌 3상을 표시할 것임
 direc = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
 snake = deque()
-snake.append((0, 0))
+snake.append((0, 0)) # 뱀의 정보를 deque로 저장
 
-x, y = 0, 0
+x, y = 0, 0 # 초기 머리의 x, y 좌표
 while 1:
-   time += 1
-   # print(f'=========time:{time}=========')
-   # print(f'dix:{dix}')
-   bx, by = x, y
-   x += direc[dix][0]
-   y += direc[dix][1]
-   if N > x >= 0 and N > y >= 0:
-      if (x, y) in snake:
+   time += 1 # 시간 증가
+
+   x += direc[dix][0] # 머리 x 좌표 이동
+   y += direc[dix][1] # 머리 y 좌표 이동
+
+   if N > x >= 0 and N > y >= 0: # 범위 안쪽이면
+      if (x, y) in snake: # 이동한 머리가 snake deque 안에 있으면 종료
          print(time)
          quit()         
 
-      if base[x][y] == 2:
-         base[x][y] = 0
-         snake.appendleft((x, y))
-      elif base[x][y] == 0:
-         snake.pop()
-         snake.appendleft((x, y))
+      # 머리 위치에 따른 몸 길이 증식 판단
+      if base[x][y] == 2: # 머리가 사과위치라면
+         base[x][y] = 0 # 사과를 없애고
+         snake.appendleft((x, y)) # 맨 앞에 머리만 추가 (몸 길이 증가)
+      elif base[x][y] == 0: # 아무것도 없다면
+         snake.pop() # 맨 뒤를 빼고
+         snake.appendleft((x, y)) # 맨 앞에 머리 추가
 
-      # for aaa in base:
-      #    print(aaa)
-      # print(f'snake:{snake}')
-
+      # 방향전환 판단
+      # infomation에 정보가 있고, 맨 앞이 현재 시간과 같을 때
       if info and info[0][0] == time:
-         # print("정보에 걸림")
-         n, d = info.pop(0)
-         # print(n, d)
-         if d == "L":
+         n, d = info.pop(0) # 맨 앞을 빼고
+         if d == "L": # 왼쪽으로 틀어라
             dix -= 1
             if dix == -1:
                dix = 3
-         else:
+         else: # 오른쪽으로 틀어라
             dix += 1
             if dix == 4:
                dix = 0
    else:
-      print(time)
+      print(time) # 범위 밖이면 종료
       quit()
