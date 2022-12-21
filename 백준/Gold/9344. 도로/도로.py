f9f0b01992d61492edba9aa70cb332d772d3f8c5
kimdongjun <cjfal2@kakao.com>
2022-12-21 09:59:01 +0900

    [Gold III] Title: 도로, Time: 892 ms, Memory: 33904 KB -BaekjoonHub

---
+++ 백준/Gold/9344. 도로/도로.py
@@ -0,0 +1,32 @@
+def find_set(x):
+    while x != arr[x]:
+        x = arr[x]
+    return x
+
+for _ in range(int(input())):
+   N, M, p, q = map(int, input().split())
+   edge = []
+   for _ in range(M):
+      u, v, w = map(int, input().split())
+      edge.append((w, u, v))
+
+   edge.sort()
+
+   arr = [i for i in range(N+1)]
+
+   N = N+1
+   cnt = 0
+   total = 0
+   spann = []
+   for w, u, v in edge:
+      if find_set(u) != find_set(v):
+         spann.append((u, v))
+         cnt += 1
+         total += w
+         arr[find_set(v)] = find_set(u)
+         if cnt == N:
+               break
+   if (p, q) in spann: 
+      print('YES')
+   else:
+      print('NO')
