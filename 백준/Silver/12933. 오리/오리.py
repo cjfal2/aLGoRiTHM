89792fa21e69268a705b2b77f3754a137508f2b5
kimdongjun <cjfal2@kakao.com>
2022-12-22 22:46:28 +0900

    [Silver III] Title: 오리, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.py
+++ 백준/Silver/12933. 오리/오리.py
@@ -3,17 +3,20 @@ if len(ducks)%5:
     print(-1)
     quit()
 
-duct = {'q': 0,'u': 1,'a': 2,'c': 3,'k': 4}
-ori = [0 for _ in range(500)]
-ans = []
-for sori in ducks:
-    for i in range(500):
-        if ori[i] == duct.get(sori):
-            ori[i] += 1
-            if ori[i] == 5:
-                ori[i] = 0
-                if i not in ans:
-                    ans.append(i)
-            break
+def duck():
+    duct = {'q': 0,'u': 1,'a': 2,'c': 3,'k': 4}
+    ori = [0 for _ in range(500)]
+    ans = []
+    for sori in ducks:
+        for i in range(500):
+            if ori[i] == duct.get(sori):
+                ori[i] += 1
+                if ori[i] == 5:
+                    ori[i] = 0
+                    if i not in ans:
+                        ans.append(i)
+                break
 
-print(len(ans)) if len(ans) and len(set(ori)) == 1 else print(-1)
+    print(len(ans)) if len(ans) and len(set(ori)) == 1 else print(-1)
+    return
+duck()
