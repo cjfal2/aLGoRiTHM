ac6af02f829e6e4dca1bd6350e789c9baa2805df
kimdongjun <cjfal2@kakao.com>
2023-01-07 11:16:28 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 30744 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,22 +1,20 @@
 import sys
-input = sys.stdin.readline
-
-N = int(input())
-W = [int(input()) for _ in range(N)]
-if N < 3:
+input=sys.stdin.readline
+N=int(input())
+W=[int(input()) for _ in range(N)]
+if N<3:
     print(sum(W))
     quit()
-def dp():
-    memo = [0 for _ in range(N)]
-    memo[0] = W[0]
-    memo[1] = sum(W[:2])
-    memo[2] = max(W[0] + W[2], W[1] + W[2], memo[1])
-
-    for i in range(3, N):
-        memo[i] = max(
-            memo[i - 3] + W[i - 1] + W[i],
-            memo[i - 2] + W[i],
-            memo[i - 1]
+def d():
+    M=[0 for _ in range(N)]
+    M[0]=W[0]
+    M[1]=sum(W[:2])
+    M[2]=max(W[0]+W[2],W[1]+W[2],M[1])
+    for i in range(3,N):
+        M[i] = max(
+            M[i-3]+W[i-1]+W[i],
+            M[i-2]+W[i],
+            M[i-1]
         )
-    print(memo[-1])
-dp()
+    print(M[-1])
+d()
