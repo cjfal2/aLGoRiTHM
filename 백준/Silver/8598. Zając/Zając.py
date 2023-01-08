00b0b690c50e8283f97aacd8cf3acd64d849e837
kimdongjun <cjfal2@kakao.com>
2023-01-08 11:00:36 +0900

    [Silver I] Title: Zając, Time: 376 ms, Memory: 134212 KB -BaekjoonHub

--- 백준/Silver/8598. Zając/Zając.py
+++ 백준/Silver/8598. Zając/Zając.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -8,26 +9,27 @@ pan = []
 flag = True
 
 for i in range(N):
-    pp = list(input().strip())
+    pp = input().strip()
     if flag:
         for j in range(M):
             if pp[j] == 'z':
                 sx, sy = i, j
                 flag = False
-                q = [(i, j)]
+                q = deque()
+                q.append((i, j))
                 visited = [[0 for _ in range(M)] for _ in range(N)]
                 visited[i][j] = 1
                 break
     pan.append(pp)
 
 while q:
-    x, y = q.pop(0)
-    if pan[x][y] == 'n':
-        print(visited[x][y]-1)
-        quit()
+    x, y = q.popleft()
     for dx, dy in (1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1):
         nx, ny = x + dx, y + dy
         if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny] and pan[nx][ny] != 'x':
+            if pan[nx][ny] == 'n':
+                print(visited[x][y])
+                quit()
             q.append((nx, ny))
             visited[nx][ny] = visited[x][y] + 1
 
