9f62b49713753dfd5c57a744cbb84db74c6139e8
kimdongjun <cjfal2@kakao.com>
2023-01-14 15:33:48 +0900

    [Silver I] Title: 물약 구매, Time: 3692 ms, Memory: 120304 KB -BaekjoonHub

--- 백준/Silver/24954. 물약 구매/물약 구매.py
+++ 백준/Silver/24954. 물약 구매/물약 구매.py
@@ -1,10 +1,30 @@
-from itertools import permutations
-from copy import deepcopy
+def dfs():
+    global ans
+    if len(s) == N:
+        total = 0
+        boksa = C[:]
+        for c in s:
+            total += boksa[c]
+            for p in P[c]:
+                boksa[p[0]] -= p[1]
+                if boksa[p[0]] <= 0:
+                    boksa[p[0]] = 1
+        ans = min(ans, total)
+        return
+
+    for j in range(1, N+1):
+        if not visited[j]:
+            visited[j] = 1
+            s.append(j)
+            dfs()
+            s.pop()
+            visited[j] = 0
+
 
 N = int(input()) # 물약의 종류 N개
 C = [0] + list(map(int, input().split())) # 물약의 가격
 # 물약의 할인 정보 N개
-P = [[] for _ in range(N+1)]
+P = dict()
 for i in range(1, N+1):
     A = []
     for _ in range(int(input())):
@@ -12,14 +32,7 @@ for i in range(1, N+1):
     P[i] = A
 
 ans = 9999999999
-for comb in permutations(list(range(1, N+1)), N):
-    total = 0
-    boksa = deepcopy(C)
-    for c in comb:
-        total += boksa[c]
-        for p in P[c]:
-            boksa[p[0]] -= p[1]
-            if boksa[p[0]] <= 0:
-                boksa[p[0]] = 1
-    ans = min(ans, total)
+s = []
+visited = [0 for _ in range(N+1)]
+dfs()
 print(ans)
