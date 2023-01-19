7750878c13956fc212f1c932c1e1784ccdbf859e
kimdongjun <cjfal2@kakao.com>
2023-01-19 20:18:16 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 412 ms, Memory: 38184 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -0,0 +1,38 @@
+def cal(arr):
+    temp = 0
+    for k in range(4):
+        if arr[0][k] != arr[1][k]:
+            temp += 1
+        if arr[0][k] != arr[2][k]:
+            temp += 1
+        if arr[2][k] != arr[1][k]:
+            temp += 1
+    return temp
+
+
+def dfs(start):
+    global ans
+    if len(s) == 3:
+        ans = min(ans, cal(s))
+        return
+
+    for i in range(start, N):
+        if not visited[i]:
+            visited[i] = 1
+            s.append(L[i])
+            dfs(i)
+            s.pop()
+            visited[i] = 0
+
+
+for _ in range(int(input())):
+    N = int(input())
+    L = list(input().split())
+    if N > 32:
+        print(0)
+        continue
+    s = []
+    ans = 99999999999
+    visited = [0 for _ in range(N)]
+    dfs(0)
+    print(ans)
