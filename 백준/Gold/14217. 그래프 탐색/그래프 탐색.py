34bb51dc91d9bc32cea5f801eb0ce60e1471273e
kimdongjun <cjfal2@kakao.com>
2023-01-21 23:11:32 +0900

    [Gold V] Title: 그래프 탐색, Time: 272 ms, Memory: 118320 KB -BaekjoonHub

--- 백준/Gold/14217. 그래프 탐색/그래프 탐색.py
+++ 백준/Gold/14217. 그래프 탐색/그래프 탐색.py
@@ -1,41 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-def bfs(start):
-    visited = [0 for _ in range(n+1)]
-    visited[start] = 1
-    q = [start]
-    mem = 0
+def bfs():
+    visited = [-1 for _ in range(n+1)]
+    visited[1] = 0
+    q = [1]
     while q:
-        mem += 1
-        for _ in range(len(q)):
-            w = q.pop(0)
-            for x in G[w]:
-                if x == 1:
-                    memo.append(mem)
-                    return
-                elif not visited[x]:
-                    visited[x] = 1
-                    q.append(x)
-    memo.append(-1)
+        w = q.pop(0)
+        for x in G[w]:
+            if visited[x] == -1:
+                visited[x] = visited[w] + 1
+                q.append(x)
+    return visited[1:]
 
 n, m = map(int, input().split()) # n: 도시의 수 m: 도로의 수
 G = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    G[a] += [b]
-    G[b] += [a]
+    G[a].append(b)
+    G[b].append(a)
 # print(G, "원래 G")
 for _ in range(int(input())):
     c, a, b = map(int, input().split())
     memo = [0]
     if c == 1:
-        G[a] += [b]
-        G[b] += [a]
+        G[a].append(b)
+        G[b].append(a)
     else:
-        G[a].pop(G[a].index(b))
-        G[b].pop(G[b].index(a))
+        G[a].remove(b)
+        G[b].remove(a)
     # print(G, "바뀐 G")
-    for k in range(2, n+1):
-        bfs(k)
-    print(*memo)
+    print(*bfs())
