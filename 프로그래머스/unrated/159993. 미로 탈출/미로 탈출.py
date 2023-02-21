db57800a5e00e35f0142d7f24eab8c742689f26f
kimdongjun <cjfal2@kakao.com>
2023-02-21 22:11:29 +0900

    [unrated] Title: 미로 탈출, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -2,24 +2,7 @@ from collections import deque
 
 def solution(maps):
     
-    def find_lever(n, m):
-        q = deque([(n, m)])
-        visited = [[0 for _ in range(M)] for _ in range(N)]
-        visited[n][m] = 1
-        while q:
-            x, y = q.popleft()
-            for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-                nx, ny = x + dx, y + dy
-                if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny] and maps[nx][ny] != "X":
-                    if maps[nx][ny] == 'L':
-                        return nx, ny, visited[x][y]
-                    visited[nx][ny] = visited[x][y] + 1
-                    q.append((nx, ny))
-        return -1, -1, -1
-
-
-
-    def find_exit(n, m, dist):
+    def find(n, m, dist, what):
         q = deque([(n, m)])
         visited = [[0 for _ in range(M)] for _ in range(N)]
         visited[n][m] = dist
@@ -28,20 +11,20 @@ def solution(maps):
             for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
                 nx, ny = x + dx, y + dy
                 if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny] and maps[nx][ny] != "X":
-                    if maps[nx][ny] == 'E':
-                        return visited[x][y] + 1
+                    if maps[nx][ny] == what:
+                        return nx, ny, visited[x][y]
                     visited[nx][ny] = visited[x][y] + 1
                     q.append((nx, ny))
-        return -1
+        return -1, -1, -1
         
 
     N = len(maps)
     M = len(maps[0])
-        
     for n in range(N):
         for m in range(M):
             if maps[n][m] == 'S':
-                i, j, distance = find_lever(n, m)
+                i, j, distance = find(n, m, 1, 'L')
                 if i == j == distance == -1:
                     return -1
-                return find_exit(i, j, distance)
+                i, j, distance = find(i, j, distance, 'E')
+                return distance + 1 if i != -1 else -1
