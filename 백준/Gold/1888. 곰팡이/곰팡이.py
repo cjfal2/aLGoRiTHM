a9bcbc4f4d611af21b928c568b83f77eecbb45e6
kimdongjun <cjfal2@kakao.com>
2023-02-24 16:16:08 +0900

    [Gold IV] Title: 곰팡이, Time: 208 ms, Memory: 34240 KB -BaekjoonHub

--- 백준/Gold/1888. 곰팡이/곰팡이.py
+++ 백준/Gold/1888. 곰팡이/곰팡이.py
@@ -1,61 +1,58 @@
-# 곰팡이가 피어있는 곳은 그 곰팡이의 자라는 속도
-# 어느 곰팡이의 자라는 속도가 k라면, 하루가 지났을 때 그 곰팡이가 피어있던 자리를 중심으로 2k+1행 2k+1열의 격자에 같은 종의 곰팡이가 번진다는 의미이다.
-# 만약 서로 다른 종의 곰팡이가 같은 칸에 번져 오면, 자라는 속도가 빠른 곰팡이가 그 칸을 차지한다.
-# 곰팡이가 한 덩어리가 되기까지 걸리는 시간을 하루 단위로 출력한다.
-from collections import deque
+from collections import defaultdict
 
 N, M = map(int, input().split())
-pan = [list(map(int, input())) for _ in range(N)]
+wall = [list(map(int, input())) for _ in range(N)]
+total = N * M
+mold = defaultdict(list)
 
-def check():
-    '''
-    덩어리를 확인할 함수
-    '''
-    number = 0
-    visited = [[0 for _ in range(M)] for _ in range(N)]
-    for n in range(N):
-        for m in range(M):
-            if not visited[n][m] and pan[n][m]:
-                number += 1
-                if number > 1: # 군집이 한 개가 넘어가면 바로 끊어줌
+def bfs(_i, _j):
+    global mold
+    q = [(_i, _j)]
+    while q:
+        _r, _c = q.pop(0)
+        for _dr, _dc in (-1, 0), (1, 0), (0, 1), (0, -1):
+            nr, nc = _r + _dr, _c + _dc
+            if -1 < nr < N and -1 < nc < M and wall[nr][nc]:
+                if visit[nr][nc] < day:
+                    visit[nr][nc] = day
+                    q.append((nr, nc))
 
+def isLump():
+    global mold
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if wall[i][j] and visit[i][j] != day:
+                if cnt:
                     return False
-                q = deque([(n, m)])
-                visited[n][m] = 1
-                while q:
-                    x, y = q.popleft()
-                    for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-                        nx, ny = x + dx, y + dy
-                        if N > nx >= 0 and M > ny >= 0 and not visited[nx][ny] and pan[nx][ny]:
-                            visited[nx][ny] = 1
-                            q.append((nx, ny))
-    return True # 군집이 한 개 이하라면 여기까지 옴
+                cnt += 1
+                visit[i][j] = day
+                bfs(i, j)
+    return True
 
-def bfs(i, j, velo):
-    '''
-    속도로 퍼트릴 함수
-    속도 이하라면 잡아먹는다.
-    '''
-    for n in range(i-velo, i+velo+1):
-        for m in range(j-velo, j+velo+1):
-            # print(n, m)
-            if N > n >= 0 and M > m >= 0 and pan[n][m] < velo:
-                pan[n][m] = velo
-                visit[n][m] = 1
+day = 0
+visit = [[-1] * M for _ in range(N)]
 
-ans = 0
-while 1:
-    if not ans and check():
-        print(ans)
-        break
-    ans += 1
-    visit = [[0 for _ in range(M)] for _ in range(N)]
-    for p in range(N):
-        for q in range(M):
-            if pan[p][q] and not visit[p][q]:
-                visit[p][q] = 1
-                bfs(p, q, pan[p][q])
+for i in range(N):
+    for j in range(M):
+        if wall[i][j]:
+            mold[wall[i][j]].append((i, j))
 
-    if check():
-        print(ans)
-        break
+while not isLump():
+
+    mold = dict(sorted(mold.items(), reverse=1))
+
+    for v, q in mold.items():
+        new_q = []
+        while q:
+            r, c = q.pop(0)
+            for nr in range(0 if (-1)*v+r <= 0 else (-1)*v+r, N if v+r >= N else v+r+1):
+                for nc in range(0 if (-1)*v+c <= 0 else (-1)*v+c, M if v+c >= M else v+c+1):
+                    if wall[nr][nc] < v:
+                        wall[nr][nc] = v
+                        new_q.append((nr, nc))
+        mold[v] = new_q[:]
+    day += 1
+
+
+print(day)
