055b81d8f92bad6418462177f9d5fca3b8e50b7b
kimdongjun <cjfal2@kakao.com>
2023-03-02 10:08:23 +0900

    [Gold V] Title: 상범 빌딩, Time: 112 ms, Memory: 34240 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -34,13 +34,16 @@ while 1:
         break
     building = [[] for _ in range(high)]
     start = []
+    flag = True
     for u in range(high):
         for k in range(g+1):
             a = list(input().strip())
             if a == []:
                 break
-            for aa in range(len(a)):
-                if a[aa] == "S":
-                    start = (u, k, aa)
+            if flag:
+                for aa in range(len(a)):
+                    if a[aa] == "S":
+                        start = (u, k, aa)
+                        flag = False
             building[u].append(a)
     bfs(s, g)
