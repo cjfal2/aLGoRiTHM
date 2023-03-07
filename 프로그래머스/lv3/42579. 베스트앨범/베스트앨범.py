b40d8cf40fa39fb88222679163a795d2e4dd0444
kimdongjun <cjfal2@kakao.com>
2023-03-07 14:42:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,27 @@
+def solution(genres, plays):
+    answer = []
+    info = dict()
+    N = len(genres)
+    
+    for idx in range(N):
+        gen = genres[idx]
+        
+        if gen not in info:
+            info[gen] = [plays[idx], [(plays[idx], idx)]]
+        else:
+            info[gen][0] += plays[idx]
+            info[gen][1].append((plays[idx], idx))
+            info[gen][1].sort(reverse=True)
+    
+    for x, arr in sorted(info.values(), reverse=True):
+        if len(arr) == 1:
+            answer.append(arr[0][1])
+        else:
+            a = arr[0]
+            b = arr[1]
+            if a[0] == b[0] and b[1] < a[1]:
+                a, b = b, a
+            answer.append(a[1])
+            answer.append(b[1])
+    
+    return answer
