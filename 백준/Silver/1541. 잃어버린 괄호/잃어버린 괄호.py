4bccaaeb52e85e6311455f1fc5bb9600f61b28ac
kimdongjun <cjfal2@kakao.com>
2023-03-19 13:58:18 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,42 +1,41 @@
 sik = input()
 
-if '-' in sik and '+' in sik:
-    sik = sik.split('-')
-    co = 0
-    if '+' in sik[0]:
-        pq = 0
-        H = sik[0].split('+')
-        for h in H:
-            pq += int(h)
-        co += pq
+sik = sik.split('-')
+co = 0
+if '+' in sik[0]:
+    temp1 = 0
+    H = sik[0].split('+')
+    for h in H:
+        temp1 += int(h)
+    co += temp1
+else:
+    co += int(sik[0])
+
+for i in range(1, len(sik)):
+    if '+' in sik[i]:
+        temp2 = 0
+        Q = sik[i].split('+')
+        for q in Q:
+            temp2 += int(q)
+        co -= temp2
     else:
-        co += int(sik[0])
-
-    for i in range(1, len(sik)):
-        if '+' in sik[i]:
-            pl = 0
-            Q = sik[i].split('+')
-            for q in Q:
-                pl += int(q)
-            co -= pl
-        else:
-            co -= int(sik[i])
-
-
-elif '-' in sik:
-    sik = sik.split('-')
-    co = int(sik[0])
-    for i in range(1, len(sik)):
         co -= int(sik[i])
 
 
-elif '+' in sik:
-    sik = sik.split('+')
-    co = int(sik[0])
-    for i in range(1, len(sik)):
-        co += int(sik[i])
+# elif '-' in sik:
+#     sik = sik.split('-')
+#     co = int(sik[0])
+#     for i in range(1, len(sik)):
+#         co -= int(sik[i])
 
-        
-else:
-    co = int(sik)
+
+# elif '+' in sik:
+#     sik = sik.split('+')
+#     co = int(sik[0])
+#     for i in range(1, len(sik)):
+#         co += int(sik[i])
+
+
+# else:
+#     co = int(sik)
 print(co)
