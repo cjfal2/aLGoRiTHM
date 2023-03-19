b0ffee0cbbc76ba4765e7880560e0a3ded08a65e
kimdongjun <cjfal2@kakao.com>
2023-03-19 14:18:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,41 +1,12 @@
-sik = input()
+def cal(formula):
+    cnt = 0
+    for num in formula.split("+"):
+        cnt += int(num)
+    return cnt
 
-sik = sik.split('-')
-co = 0
-if '+' in sik[0]:
-    temp1 = 0
-    H = sik[0].split('+')
-    for h in H:
-        temp1 += int(h)
-    co += temp1
-else:
-    co += int(sik[0])
 
+sik = input().split('-')
+co = cal(sik[0])
 for i in range(1, len(sik)):
-    if '+' in sik[i]:
-        temp2 = 0
-        Q = sik[i].split('+')
-        for q in Q:
-            temp2 += int(q)
-        co -= temp2
-    else:
-        co -= int(sik[i])
-
-
-# elif '-' in sik:
-#     sik = sik.split('-')
-#     co = int(sik[0])
-#     for i in range(1, len(sik)):
-#         co -= int(sik[i])
-
-
-# elif '+' in sik:
-#     sik = sik.split('+')
-#     co = int(sik[0])
-#     for i in range(1, len(sik)):
-#         co += int(sik[i])
-
-
-# else:
-#     co = int(sik)
+    co -= cal(sik[i])
 print(co)
