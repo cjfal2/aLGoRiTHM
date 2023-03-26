6e0de7b07a8fc65443707252e7f3578ac0649b9a
kimdongjun <cjfal2@kakao.com>
2023-03-26 13:05:23 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -1,7 +1,7 @@
 a={
-    'NLCS': 'North London Collegiate School',
-    'BHA': 'Branksome Hall Asia',
-    'KIS': 'Korea International School',
-    'SJA': 'St. Johnsbury Academy'
+    'NLCS':'North London Collegiate School',
+    'BHA':'Branksome Hall Asia',
+    'KIS':'Korea International School',
+    'SJA':'St. Johnsbury Academy'
 }
 print(a[input()])
