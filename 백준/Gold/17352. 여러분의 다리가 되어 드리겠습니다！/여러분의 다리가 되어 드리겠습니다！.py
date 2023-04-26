23b4b3cf4f1fe5b784c3047b209c92c17b6cb3a4
kimdongjun <cjfal2@kakao.com>
2023-04-26 21:24:59 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 632 ms, Memory: 43012 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000000)
+
+
+def find(x):
+    if rep[x] != x:
+        rep[x] = find(rep[x])
+    return rep[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x < y:
+        rep[y] = x
+    else:
+        rep[x] = y
+
+
+N = int(input().strip())
+rep = [n for n in range(N+1)]
+
+for i in range(N-2):
+    a, b = map(int, input().strip().split())
+    if find(a) != find(b):
+        union(a, b)
+
+leaves = [i for i in range(1, N+1) if rep[i] == i]
+
+for leaf in leaves:
+    print(leaf, end=" ")
+print()
