7a9dd3bd4a32e7af6b8d04a09d702e6ac6ae10f3
kimdongjun <cjfal2@kakao.com>
2023-05-10 08:38:03 +0900

    [Gold III] Title: 세 용액, Time: 276 ms, Memory: 115728 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def mixing(x):
+    '''
+    pH: 섞은 용액의 가장 0에 가까운 특성 값
+    basic: -쪽 용액의 포인터 염기성
+    acidic: +쪽 용액의 포인터 산성
+    '''
+    pH, basic, acidic = sys.maxsize, liquid.index(x)+1, N - 1
+    while (basic < acidic): # 염기성이 산성보다 커지면 종료
+        mix = x + liquid[basic] + liquid[acidic] # 염기성과 산성을 섞어봄
+
+        if pH > abs(mix): # pH 보다 mix가 0에 가까우면
+            pH, neutral_small, neutral_large = abs(mix), liquid[basic], liquid[acidic]
+            # pH를 갱신하고, 중성작은거, 중성큰거를 저장
+            if not mix: # 만약 0이면 바로 종료
+                break
+
+        if mix < 0: # mix가 0보다 작으면 염기성 포인터를 옮겨봄
+            basic += 1
+        else: # mix가 0보다 크면 산성 포인터를 옮겨봄
+            acidic -= 1
+    return pH, x, neutral_small, neutral_large
+
+
+# 인풋 받고 바로 솔트 때려주고요
+N = int(input())
+liquid = sorted(map(int, input().split()))
+if liquid[0] >= 0:
+    print(*liquid[:3])
+    quit()
+if liquid[-1] <= 0:
+    print(*liquid[N-3:])
+    quit()
+
+ans = []
+temp = sys.maxsize
+for x in liquid[:N-2]:
+    a, b, c, d = mixing(x)
+    if temp > a:
+        temp = a
+        ans = [b, c, d]
+print(*ans) # 중성작은거순으로 출력
