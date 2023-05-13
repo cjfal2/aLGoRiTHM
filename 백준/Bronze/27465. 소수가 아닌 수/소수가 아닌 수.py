fb6a1751285d2d2ec1aadbe2ac767cafa9af1ce0
kimdongjun <cjfal2@kakao.com>
2023-05-14 08:56:47 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -1,12 +1,1 @@
-def check(num):
-    for m in range(2, int(num ** 0.5) + 1):
-        if not num % m:     
-            return True
-    return False
-
-n = int(input())
-while 1:
-    n += 1
-    if check(n):
-        print(n)
-        break
+print(10**9)
