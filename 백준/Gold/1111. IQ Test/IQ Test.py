666a857ce8bd63ae9b1b1ee18dd4885715e21835
kimdongjun <cjfal2@kakao.com>
2023-07-18 21:35:25 +0900

    [Gold III] Title: IQ Test, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1111. IQ Test/IQ Test.py
+++ 백준/Gold/1111. IQ Test/IQ Test.py
@@ -1,4 +1,5 @@
 N = int(input())
+
 if N == 1:
     print("A")
     quit()
@@ -9,21 +10,12 @@ if N == 2:
     print("A") if L[0] != L[1] else print(L[0])
     quit()
 
-
-
 num1, num2, num3 = L[0], L[1], L[2]
-
 temp1 = num2 - num1
-if temp1 != 0:
-    a = (num3 - num2) // (num2 - num1)
-else:
-    a = 0
-
+a = (num3 - num2) // (num2 - num1) if temp1 != 0 else 0
 b = num2 - num1 * a
-
-
 for n in range(2, N):
     if L[n-1] * a + b != L[n]:
         print("B")
         quit()
-print(int(L[-1] * a + b))
+print(L[-1] * a + b)
