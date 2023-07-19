b7c6a1d81df1968279b255c9ce7ad0043aedafcd
kimdongjun <cjfal2@kakao.com>
2023-07-19 16:38:28 +0900

    [Bronze III] Title: 네 수, Time: 140 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `10 20 30 40
+`
+)
+  .trim()
+  .split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const [A, B, C, D] = input().trim().split(" ");
+const num1 = parseInt(A + B)
+const num2 = parseInt(C + D)
+
+function sum(x) {
+  const add = function(y) {
+    return x + y
+  }
+  return add
+}
+
+const answer = sum(num1)
+console.log(answer(num2))
