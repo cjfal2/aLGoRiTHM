177dda67a063cf231946af854e42ec5827d078a9
kimdongjun <cjfal2@kakao.com>
2023-07-24 23:32:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    const answer = [];
+    
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const sum = numbers[i] + numbers[j]
+            if (!answer.includes(sum)) answer.push(sum)
+        }
+    }
+    answer.sort((a, b) => a - b)
+    return answer;
+}
