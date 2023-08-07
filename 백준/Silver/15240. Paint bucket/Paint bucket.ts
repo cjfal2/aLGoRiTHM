88fe829044f5afc90b369d7ae7fab6f7090be57c
kimdongjun <cjfal2@kakao.com>
2023-08-07 09:53:35 +0900

    [Silver I] Title: Paint bucket, Time: 644 ms, Memory: 60972 KB -BaekjoonHub

--- 백준/Silver/15240. Paint bucket/Paint bucket.ts
+++ 백준/Silver/15240. Paint bucket/Paint bucket.ts
@@ -34,6 +34,7 @@ for (let t = 0; t < N; t++) {
 const [n, m, w]: number[] = input().trim().split(" ").map(Number);
 const f: number = paint[n][m];
 paint[n][m] = w;
+visited[n][m] = true;
 const queue: number[][] = [[n, m]];
 while (queue.length > 0) {
   const q: number[] | undefined = queue.shift(); // 배열 q가 비어있으면 undefined이므로 두개를 선언해줘야한다. 하지만 없는경우는 없다.
@@ -41,17 +42,22 @@ while (queue.length > 0) {
     // 없는경우가 있을 수 있다는 멍청한 타입스크립트 때문에 if로 값이 있을 때만 해줘야 한다.
     const x: number = q[0];
     const y: number = q[1];
-    if (!visited[x][y]) {
-      visited[x][y] = true;
-      directions.forEach((d) => {
-        const nx: number = x + d[0];
-        const ny: number = y + d[1];
-        if (N > nx && nx >= 0 && M > ny && ny >= 0 && paint[nx][ny] === f) {
-          paint[nx][ny] = w;
-          queue.push([nx, ny]);
-        }
-      });
-    }
+    directions.forEach((d) => {
+      const nx: number = x + d[0];
+      const ny: number = y + d[1];
+      if (
+        N > nx &&
+        nx >= 0 &&
+        M > ny &&
+        ny >= 0 &&
+        paint[nx][ny] === f &&
+        !visited[nx][ny]
+      ) {
+        paint[nx][ny] = w;
+        visited[nx][ny] = true;
+        queue.push([nx, ny]);
+      }
+    });
   }
 }
 
