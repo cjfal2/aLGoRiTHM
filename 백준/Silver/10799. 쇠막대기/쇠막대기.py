adf2225a4879616d368c22af9b9e56b2fa93c862
kimdongjun <cjfal2@kakao.com>
2023-08-25 10:12:54 +0900

    [Silver II] Title: 쇠막대기, Time: 132 ms, Memory: 119776 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,21 +1,21 @@
 def LEN(arr):
     co = 0
     for _ in arr:
-        co+=1
+        co += 1
     return co
 
 L = list(input())
-if LEN(L) == 2:
-    print(0)
-else:
-    stick = 0
-    zogak = 1
-    for i in range(LEN(L)-1):
-        if L[i] =='(' and L[i+1] == ')':
-            zogak += stick
-        elif L[i] == '(' and L[i+1] != ')':
-            stick += 1
-        elif i>1 and L[i] == ')' and L[i-1] != '(':
-            stick -= 1
-            zogak += 1
-    print(zogak)
+stick = 0
+zogak = 0
+
+for i in range(LEN(L)):
+    if L[i] == '(':
+        stick += 1
+    elif L[i] == ')' and L[i - 1] == '(':
+        stick -= 1
+        zogak += stick
+    elif L[i] == ')' and L[i - 1] != '(':
+        stick -= 1
+        zogak += 1
+
+print(zogak)
