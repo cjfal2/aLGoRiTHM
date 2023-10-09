fe9884b136aa85a891c655f8ec93d7627f1830a5
kimdongjun <cjfal2@kakao.com>
2023-10-09 23:16:16 +0900

    [Silver I] Title: 스타트와 링크, Time: 2688 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,22 +1,30 @@
-from itertools import combinations
+def dfs(q):
+    global answer
+
+    if len(start) == N//2:
+        link = []
+        for x in range(N):
+            if x not in start:
+                link.append(x)
+
+        t, k = 0, 0
+        for i in range(N//2 - 1):
+            for j in range(i+1, N//2):
+                t += pan[start[i]][start[j]] + pan[start[j]][start[i]]
+                k += pan[link[i]][link[j]] + pan[link[j]][link[i]]
+        answer = min(answer, abs(t-k))
+        return
+
+    for i in range(q, N):
+        if i not in start:
+            start.append(i)
+            dfs(i)
+            start.pop()
 
 
 N = int(input())
 pan = [list(map(int, input().split())) for _ in range(N)]
+start = []
 answer = float("INF")
-
-for start in combinations(list(range(N)), N//2):
-    link = []
-    for s in range(N):
-        if s not in start:
-            link.append(s)
-
-    t, k = 0, 0
-    for i in range(N//2 - 1):
-        for j in range(i+1, N//2):
-            t += pan[start[i]][start[j]] + pan[start[j]][start[i]]
-            k += pan[link[i]][link[j]] + pan[link[j]][link[i]]
-            
-    answer = min(answer, abs(t-k))
-
+dfs(0)
 print(answer)
