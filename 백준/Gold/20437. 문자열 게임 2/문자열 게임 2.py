d6c09e69c4fc6b8e0f4c20e51195baa3a74a07ca
kimdongjun <cjfal2@kakao.com>
2023-11-21 18:58:30 +0900

    [Gold V] Title: 문자열 게임 2, Time: 508 ms, Memory: 111928 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -1,29 +1,35 @@
-from collections import defaultdict
+import sys
+input = sys.stdin.readline
 
-
-for _ in range(int(input())):
-    w, k = input(), int(input())
-    if k == 1:
+for _ in range(int(input().strip())):
+    W, K = input().strip(), int(input().strip())
+    if K == 1:
         print(1, 1)
         continue
+    
+    # 알파벳 문자별 등장 빈도수 저장
+    count_w = [0 for _ in range(26)]
+    for what in W:
+        count_w[ord(what) - 97] += 1
 
-    long = len(w)
-    MIN = long
-    MAX = 0
-    memo = defaultdict(list)
+    long = len(W) # 문자열의 길이
+    MIN = long+1 # 최소값
+    MAX = -1 # 최대값
+    for i in range(long-1): # 처음부터 (끝 - 1) 까지 탐색
+        # 빈도수가 K 이상인 문자만 이용
+        if count_w[ord(W[i]) - 97] >= K:
+            cnt = 0
+            for j in range(i, long): # i 부터 끝까지 탐색
+                # 연속 문자열의 시작 문자 W[i]가 W[j] 와 같으면 카운팅
+                if W[i] == W[j]:
+                    cnt += 1
 
-    for i in range(long):
-        if w.count(w[i]) >= k:
-            memo[w[i]].append(i)
+                if cnt == K:  # 카운트 수가 정확히 K와 같으면 길이 업데이트하고 탈출
+                    MIN = min(MIN, j - i + 1)
+                    MAX = max(MAX, j - i + 1)
+                    break
 
-    if not memo:
+    if MIN == long+1 or MAX == -1: # 변한게 없다면
         print(-1)
-        continue
-
-    for i in memo.values():
-        for j in range(len(i)-k+1):
-            temp = i[j+k-1]-i[j]+1
-            MIN = min(MIN, temp)
-            MAX = max(MAX, temp)
-
-    print(MIN, MAX)
+    else:
+        print(MIN, MAX)
