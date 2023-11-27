b8f9b97348d23f82703a7935766d506e7c6b94d0
kimdongjun <cjfal2@kakao.com>
2023-11-27 09:14:19 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.c
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.c
@@ -2,7 +2,7 @@
 
 int main(void) {
   char a;
-  scanf(" %c", &a); // 공백을 추가하여 개행 문자를 무시합니다.
+  scanf("%c", &a); // 공백을 추가하여 개행 문자를 무시합니다.
 
   if (a == 'M') {
     printf("%s\n", "MatKor"); // 문자열을 출력하기 위해서는 %s를 사용합니다.
