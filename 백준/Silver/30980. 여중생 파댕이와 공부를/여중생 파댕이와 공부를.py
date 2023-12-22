567242cd2fdd7fbae9c17288bf045869b175316d
kimdongjun <cjfal2@kakao.com>
2023-12-22 12:41:02 +0900

    [Silver III] Title: 여중생 파댕이와 공부를, Time: 84 ms, Memory: 31744 KB -BaekjoonHub

--- 백준/Silver/30980. 여중생 파댕이와 공부를/여중생 파댕이와 공부를.py
+++ 백준/Silver/30980. 여중생 파댕이와 공부를/여중생 파댕이와 공부를.py
@@ -1,20 +1,16 @@
-def ox_func(n, ms, me):
-    hap = int(pan[n][me]) if me % 2 else int("".join(pan[n][me-1:me+1]))
-
-    if int(pan[n][ms]) + int(pan[n][ms+2]) == hap:
-        pan[n][ms-1] = pan[n][me+1] = "*"
-        for k in range(ms, me+1):
-            pan[n-1][k] = pan[n+1][k] = "*"
+def x(n,t,p):
+    hap=int(v[n][p]) if p % 2 else int("".join(v[n][p-1:p+1]))
+    if int(v[n][t])+int(v[n][t+2])==hap:
+        v[n][t-1]=v[n][p+1]="*"
+        for k in range(t,p+1):
+            v[n-1][k]=v[n+1][k]="*"
     else:
-        pan[n-1][ms+2] = pan[n][ms+1] = pan[n+1][ms] = "/"
-
-
-N, M = map(int, input().split())
-pan = [list(input()) for _ in range(N*3)]
-
-for i in range(1, 3*N, 3):
-    for j in range(1, 8*M, 8):
-        ox_func(i, j, j+4) if pan[i][j+5] == "." else ox_func(i, j, j+5)
-
-    for a in range(i-1, i+2):
-        print(*pan[a], sep="")
+        v[n-1][t+2]=v[n][t+1]=v[n+1][t]="/"
+        
+N, M=map(int,input().split())
+v=[list(input()) for _ in range(N*3)]
+for i in range(1,3*N,3):
+    for j in range(1,8*M,8):
+        x(i,j,j+4) if v[i][j+5]=="." else x(i,j,j+5)
+    for a in range(i-1,i+2):
+        print(*v[a],sep="")
