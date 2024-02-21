a01410cc6c373bdaca25b94a74b6d731b129d61f
kimdongjun <cjfal2@kakao.com>
2024-02-22 08:35:24 +0900

    [Bronze III] Title: 앵그리 창영, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.py
@@ -1,9 +1,3 @@
 N, garo, sero = map(int, input().split())
-daegak = garo**2 + sero**2
-
 for _ in range(N):
-    k = int(input())
-    if k * k <= daegak:
-        print("DA")
-    else:
-        print("NE")
+    print("DA" if int(input()) ** 2 <= garo**2 + sero**2 else "NE")
