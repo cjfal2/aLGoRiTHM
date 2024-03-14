68f01543b798d2419bde829a6f6a8a3415242869
kimdongjun <cjfal2@kakao.com>
2024-03-14 09:46:56 +0900

    [Gold IV] Title: 지뢰찾기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2140. 지뢰찾기/지뢰찾기.py
+++ 백준/Gold/2140. 지뢰찾기/지뢰찾기.py
@@ -1,17 +1,17 @@
-N = int(input())
-pan = [list(map(lambda x: int(x) if x != "#" else x, list(input()))) for _ in range(N)]
-dix = ((1, 0), (0, 1), (-1, 0), (0, -1), (-1, -1), (1, 1), (-1, 1), (1, -1))
-answer = 0 if N <= 4 else (N-4) ** 2
-for x in range(1, N-1):
-    for y in range(1, N-1):
-        if x in (1, N-2) or y in (1, N-2):
-            for dx, dy in dix:
-                if pan[x + dx][y + dy] == 0:
+N=int(input())
+p=[list(map(lambda x: int(x) if x != "#" else x,list(input()))) for _ in range(N)]
+d=((1,0),(0,1),(-1,0),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1))
+a=0 if N<=4 else (N-4)**2
+for x in range(1,N-1):
+    for y in range(1,N-1):
+        if x in (1,N-2)or y in (1,N-2):
+            for i,j in d:
+                if p[x+i][y+j]==0:
                     break
             else:
-                for dx, dy in dix:
-                    nx, ny = x + dx, y + dy
-                    if pan[nx][ny] != "#":
-                        pan[nx][ny] -= 1
-                answer += 1
-print(answer)
+                for i,j in d:
+                    nx,ny=x+i,y+j
+                    if p[nx][ny]!="#":
+                        p[nx][ny]-=1
+                a+=1
+print(a)
