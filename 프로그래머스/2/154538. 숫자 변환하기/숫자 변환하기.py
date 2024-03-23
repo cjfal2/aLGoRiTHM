cbe306a63d9314f33934e3dce0bbb11eb5276c1b
kimdongjun <cjfal2@kakao.com>
2024-03-23 09:25:15 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(x, y, n):
+    q = deque([(x, 0)])
+    visited = set()
+    visited.add(x)
+    while q:
+        x, cnt = q.popleft()
+        if x == y:
+            return cnt
+        for nx in (x+n), (x*2), (x*3):
+            if nx not in visited and nx < 1000001:
+                visited.add(nx)
+                q.append((nx, cnt+1))
+    return -1
