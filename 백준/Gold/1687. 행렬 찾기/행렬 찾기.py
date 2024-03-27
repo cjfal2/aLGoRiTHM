74f4b9eae80af944259688bd099428e5bb7d4ce1
kimdongjun <cjfal2@kakao.com>
2024-03-27 11:41:43 +0900

    [Gold III] Title: 행렬 찾기, Time: 492 ms, Memory: 112384 KB -BaekjoonHub

--- 백준/Gold/1687. 행렬 찾기/행렬 찾기.py
+++ 백준/Gold/1687. 행렬 찾기/행렬 찾기.py
@@ -1,30 +1,20 @@
-n, m = map(int, input().split())
+N, M = map(int, input().split())
+matrix = [[0 if x == "1" else 1 for x in input()] for _ in range(N)]
+dp = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
 
-matrix = [[0] * (m + 1) for _ in range(n + 1)]
-dp = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        dp[i][j] = matrix[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
 
-for i in range(1, n + 1):
-    temp = input()
-    for j in range(1, m + 1):
-        if temp[j - 1] == '0':
-            matrix[i][j] = 1
-        elif temp[j - 1] == '1':
-            matrix[i][j] = 0
-
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i][j]
-
-ans = 0
-for i in range(1, m + 1):
-    for j in range(i, m + 1):
+answer = 0
+for i in range(1, M + 1):
+    for j in range(i, M + 1):
         cnt = 0
-        for k in range(1, n + 1):
+        for k in range(1, N + 1):
             temp = dp[k][j] - dp[k][i - 1] - dp[k - 1][j] + dp[k - 1][i - 1]
             if temp == j - i + 1:
                 cnt += temp
-                ans = max(ans, cnt)
+                answer = max(answer, cnt)
             else:
                 cnt = 0
-
-print(ans)
+print(answer)
