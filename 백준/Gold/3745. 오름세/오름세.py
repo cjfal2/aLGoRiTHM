db5498de636d6336a3bd9b0e20d1531ea71b3c2c
kimdongjun <cjfal2@kakao.com>
2024-04-04 10:08:26 +0900

    [Gold II] Title: 오름세, Time: 160 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.py
@@ -0,0 +1,33 @@
+def binary_search(A, left, right, target):
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] >= target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+
+def long(arr):
+    A = [0 for _ in range(N)]
+    A[0] = arr[0]
+    length = 1
+    for i in range(1, N):
+        if arr[i] < A[0]:
+            A[0] = arr[i]
+        elif arr[i] > A[length-1]:
+            A[length] = arr[i]
+            length += 1
+        else:
+            j = binary_search(A, 0, length-1, arr[i])
+            A[j] = arr[i]
+    return length
+
+
+while 1:
+    try:
+        N = int(input())
+        L = list(map(int, input().split()))
+        print(long(L))
+    except:
+        break
