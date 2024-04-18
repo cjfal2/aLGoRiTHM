353e696dc50a1f7a4e58bd48b5007fecf23e1cea
kimdongjun <cjfal2@kakao.com>
2024-04-18 09:09:24 +0900

    [Bronze I] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4150. 피보나치 수/피보나치 수.py
@@ -0,0 +1,30 @@
+def fibonacci(n):
+    F = [[1, 1], [1, 0]]
+    if n == 0:
+        return 0
+    power(F, n - 1)
+    return F[0][0]
+
+
+def multiply(F, M):
+    x = (F[0][0] * M[0][0] + F[0][1] * M[1][0])
+    y = (F[0][0] * M[0][1] + F[0][1] * M[1][1])
+    z = (F[1][0] * M[0][0] + F[1][1] * M[1][0])
+    w = (F[1][0] * M[0][1] + F[1][1] * M[1][1])
+    F[0][0] = x
+    F[0][1] = y
+    F[1][0] = z
+    F[1][1] = w
+
+
+def power(PF, pn):
+    if pn == 0 or pn == 1:
+        return
+    M = [[1, 1], [1, 0]]
+    power(PF, pn // 2)
+    multiply(PF, PF)
+    if pn % 2 != 0:
+        multiply(PF, M)
+
+
+print(fibonacci(int(input())))
