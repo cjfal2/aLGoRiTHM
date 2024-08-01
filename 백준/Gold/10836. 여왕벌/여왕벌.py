6ec864999ccea0661129ab8edbc5eb2498f4efed
kimdongjun <cjfal2@kakao.com>
2024-08-01 14:48:58 +0900

    [Gold IV] Title: 여왕벌, Time: 3864 ms, Memory: 116328 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,34 +1,42 @@
+import sys
+input = sys.stdin.readline
+
 M, N = map(int, input().split())
-hive = [[1 for _ in range(M)] for _ in range(M)]
+
+# 각 칸의 성장을 나타내는 리스트를 0으로 초기화
+growth = [0] * (2 * M - 1)
+
+# 입력을 받아 성장 정보를 누적
 for _ in range(N):
-    growth_record = [[0 for _ in range(M)] for _ in range(M)] # 성장도 기록
-
-    # 인풋
     zero, one, two = map(int, input().split())
-    # 자라는 정도를 리스트로 구현
-    growth = [0 for _ in range(zero)] + [1 for _ in range(one)] + [2 for _ in range(two)]
-    
-    # 가장자리의 성장 (왼쪽 밑 시작 => 위 => 오른쪽)
-    x, y = M-1, 0 # 시작 지점을 왼쪽 밑으로 설정한다.
-    
-    # growth의 인덱스를 맞춰준다.
-    for grow_idx in range(2*M-1): # 루프문을 통해서 점을 이동시키며 성장시킨다. 2M -1 만큼 이동하면 종료
-        growth_amount = growth[grow_idx]
-        hive[x][y] += growth_amount # 성장 정도 만큼 성장시킨다.
-        growth_record[x][y] += growth_amount # 성장도를 기록한다.
+    idx = 0
+    for _ in range(zero):
+        growth[idx] += 0
+        idx += 1
+    for _ in range(one):
+        growth[idx] += 1
+        idx += 1
+    for _ in range(two):
+        growth[idx] += 2
+        idx += 1
 
-        # x값이나 y값 중 하나를 옮긴다.
-        if x == 0:
-            y += 1
-        else:
-            x -= 1
-    
-    # (1, 1) 부터 (M-1, M-1) 까지 순회하면서 성장도와 hive에 기록
-    for x in range(1, M):
-        for y in range(1, M):
-            # 왼, 왼위, 위의 성장도 확인하여 가장 큰 값을 체크
-            how_here_growth = max(growth_record[x][y-1], growth_record[x-1][y-1], growth_record[x-1][y])
-            growth_record[x][y] = how_here_growth # 성장도 저장
-            hive[x][y] += how_here_growth # 벌집에 성장도 반영
-for h in hive:
-    print(*h)
+# 벌집 초기화
+hive = [[1] * M for _ in range(M)]
+
+# 가장자리 성장 적용
+index = 0
+for i in range(M-1, -1, -1):
+    hive[i][0] += growth[index]
+    index += 1
+for j in range(1, M):
+    hive[0][j] += growth[index]
+    index += 1
+
+# 내부 애벌레 성장 적용
+for i in range(1, M):
+    for j in range(1, M):
+        hive[i][j] = max(hive[i-1][j], hive[i][j-1], hive[i-1][j-1])
+
+# 결과 출력
+for row in hive:
+    print(*row)
