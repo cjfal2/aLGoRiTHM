7ec0edc935f41e9e7f34fa34b762462506d1e230
kimdongjun <cjfal2@kakao.com>
2024-08-01 14:17:59 +0900

    [Bronze IV] Title: Archivist, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,6 @@
+a = ["ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO",
+ "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO",
+ "ITMO", "PetrSU, ITMO", "SPbSU", "SPbSU",
+ "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU",
+ "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO"]
+print(a[int(input())-1995])
