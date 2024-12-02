67eb33f2e4265dc1fabcb2785271d844f2ab02f2
kimdongjun <cjfal2@kakao.com>
2024-12-02 10:17:51 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 344 ms, Memory: 70676 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,5 +1,5 @@
 N = int(input())
-memo = [1, 1] + [0 for _ in range(N)]
-for i in range(2, N):
+memo = [0, 1, 1] + [0 for _ in range(N)]
+for i in range(3, N+1):
     memo[i] = (memo[i - 1] + memo[i - 2]) % 1000000007
-print(memo[N - 1])
+print(memo[N])
