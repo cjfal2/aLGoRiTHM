37050b863908c1ad0c686884c20e1911d04c63cd
kimdongjun <cjfal2@kakao.com>
2025-01-07 10:01:59 +0900

    [Bronze IV] Title: Quadrants, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -0,0 +1,16 @@
+while 1:
+    a, b = map(float, input().split())
+    if a == b == 0:
+        print("AXIS")
+        break
+
+    if a == 0 or b == 0:
+        print("AXIS")
+    elif a > 0 and b > 0:
+        print("Q1")
+    elif a < 0 and b > 0:
+        print("Q2")
+    elif a < 0 and b < 0:
+        print("Q3")
+    else:
+        print("Q4")
