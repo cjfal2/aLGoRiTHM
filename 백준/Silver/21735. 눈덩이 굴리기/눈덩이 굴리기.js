9c230dca706b604f3a80804b3f02ea29da6ef48d
kimdongjun <cjfal2@kakao.com>
2025-02-03 21:09:32 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 96 ms, Memory: 9616 KB -BaekjoonHub

---
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const stdin = (
+    process.platform === 'linux'
+        ? fs.readFileSync('/dev/stdin').toString()
+        : `10 5
+1 3 4 5 6 7 8 10 12 14
+`
+)
+    .trim()
+    .split('\n');
+
+const input = (() => {
+    let line = 0;
+    return () => stdin[line++];
+})();
+
+const [N, M] = input().trim().split(' ').map(Number);
+const arr = input().split(' ').map(Number);
+
+let maxSize = 0;
+
+const snowballing = (now, size, time) => {
+    if (time > M) return;
+
+    if (now >= N - 1 || time === M) {
+        maxSize = Math.max(maxSize, size);
+        return;
+    }
+
+    if (now + 1 < N) {
+        const newSize = size + arr[now + 1];
+        snowballing(now + 1, newSize, time + 1);
+    }
+
+    if (now + 2 < N) {
+        const newSize = Math.floor(size / 2) + arr[now + 2];
+        snowballing(now + 2, newSize, time + 1);
+    }
+};
+
+snowballing(-1, 1, 0);
+console.log(maxSize);
